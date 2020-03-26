32af77f1fc9529897a191e004c513319dcf900e2
jddr27 <jddeleonr.inf2013@gmail.com>
2020-03-26 12:47:32 -0600

    6.1

--- modulos/cpu/201503393_cpu.c
+++ modulos/cpu/201503393_cpu.c
@@ -155,7 +155,7 @@ static int cpuinfo_proc_open(struct inode *inode, struct file *file)
         unsigned int size = 1024 + 128 * num_online_cpus();
 	/* minimum size to display an interrupt count : 2 bytes */
 	size += 2 * nr_irqs;
-	return single_open(file, cpuinfo_proc_show, NULL, size);
+	return single_open_size(file, cpuinfo_proc_show, NULL, size);
 }
 
 static const struct file_operations cpuinfo_proc_fops = {
